920f7c36c453e04e857f5a047609f12e099b6232
Jessica Kerr <jessitron@honeycomb.io>
2023-02-21 21:34:02 -0600

    Tracing for dataset operations
    this does some complicated things that only apply to honeycomb

--- src/dataset.ts
+++ src/dataset.ts
@@ -1,57 +1,86 @@
 import axios from "axios";
 import { fetchAuthorization, fetchConfiguration } from "./honeyApi";
+import otel from "@opentelemetry/api";
+
+const tracer = otel.trace.getTracer("dataset.ts");
 
 export async function initializeDataset() {
-  const { dataset, apiKey } = fetchConfiguration();
-  if (!apiKey || !dataset) {
-    return;
-  }
-  const headers = { "X-Honeycomb-Team": apiKey };
+  return tracer.startActiveSpan("initialize dataset", async (s) => {
+    const { dataset, apiKey } = fetchConfiguration();
+    s.setAttribute("app.dataset", dataset || "no dataset defined");
+    s.setAttribute("app.apiKeyLength", apiKey?.length || 0);
+    if (!apiKey || !dataset) {
+      s.setStatus({
+        code: 4,
+        message: "API or dataset not populated, can't make it cute",
+      });
+      s.end();
+      return;
+    }
+    const headers = { "X-Honeycomb-Team": apiKey };
 
-  const existingDataset = await axios
-    .get(`https://api.honeycomb.io/1/datasets/${dataset}`, {
-      headers,
-    })
-    .catch((e) => e.response);
-  if (existingDataset.status === 404) {
-    return createDataset(apiKey, dataset);
-  } else if (existingDataset.status === 200) {
-    console.log(`Dataset ${dataset} exists, ok`);
-  } else {
-    console.log(
-      "WARNING: Failed to check for dataset: " + existingDataset.status
-    );
-  }
+    const existingDataset = await axios
+      .get(`https://api.honeycomb.io/1/datasets/${dataset}`, {
+        headers,
+      })
+      .catch((e) => e.response);
+    s.setAttribute("app.resultStatus", existingDataset.status);
+    if (existingDataset.status === 404) {
+      const createResult = createDataset(apiKey, dataset);
+      s.end();
+      return createResult;
+    } else if (existingDataset.status === 200) {
+      console.log(`Dataset ${dataset} exists, ok`);
+    } else {
+      console.log(
+        "WARNING: Failed to check for dataset: " + existingDataset.status
+      );
+    }
+    s.end();
+    return;
+  });
 }
 
 const HappyO11ydaysDescription =
   "Happy O11ydays from Honeycomb! This dataset brought to you by: github.com/honeycombio/happy-o11ydays";
 
 async function createDataset(apiKey: string, dataset: string) {
-  const name = dataset === "happy-o11ydays" ? "Happy O11ydays" : dataset;
-  const headers = { "X-Honeycomb-Team": apiKey };
-  const datasetProperties = {
-    name,
-    description: HappyO11ydaysDescription,
-    slug: dataset,
-    expand_json_depth: 0,
-  };
+  return tracer.startActiveSpan("create dataset", async (s) => {
+    const name = dataset === "happy-o11ydays" ? "Happy O11ydays" : dataset;
+    const headers = { "X-Honeycomb-Team": apiKey };
+    const datasetProperties = {
+      name,
+      description: HappyO11ydaysDescription,
+      slug: dataset,
+      expand_json_depth: 0,
+    };
 
-  const response = await axios
-    .post("https://api.honeycomb.io/1/datasets", datasetProperties, { headers })
-    .catch((e) => {
-      return e.response;
-    });
-  if (response.status === 201) {
-    console.log(`Dataset ${dataset} created`);
-    // great
-  } else if (response.status === 401) {
-    console.log(`Unauthorized on create dataset :-( `);
-    const authData = fetchAuthorization(apiKey);
-    console.log(JSON.stringify(authData, null, 2));
-  } else {
-    console.log(
-      "WARNING: Could not create dataset: " + JSON.stringify(response.status)
-    );
-  }
+    const response = await axios
+      .post("https://api.honeycomb.io/1/datasets", datasetProperties, {
+        headers,
+      })
+      .catch((e) => {
+        s.setAttribute("error", true);
+        s.recordException(e);
+        return e.response;
+      });
+    s.setAttribute("app.responseStatus", response.status);
+    if (response.status === 201) {
+      console.log(`Dataset ${dataset} created`);
+      // great
+      s.setAttribute("app.datasetCreated", dataset);
+    } else if (response.status === 401) {
+      console.log(`Unauthorized on create dataset :-( `);
+      const authData = fetchAuthorization(apiKey);
+      s.setAttribute("error", true);
+      console.log(JSON.stringify(authData, null, 2)); // this must have been useful at some point
+    } else {
+      console.log(
+        "WARNING: Could not create dataset: " + JSON.stringify(response.status)
+      );
+      s.setAttribute("error", true);
+    }
+    s.end();
+    return;
+  });
 }
