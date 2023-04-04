1fa68b3166bea5d08016dd2597b5b3312020b28b
Jessica Kerr <jessitron@honeycomb.io>
2023-04-04 18:40:07 -0500

    Show line number etc

--- src/tracing.ts
+++ src/tracing.ts
@@ -5,11 +5,13 @@ const tracer = otel.trace.getTracer("not the art, but the artist");
 
 export function spaninate<R>(name: string, cb: (s: Span) => R): R {
   const stack = ErrorStackParser.parse(new Error("BOOM"));
-  const where = stack[1].source;
+  const where = stack[1];
   return tracer.startActiveSpan(name, (s) => {
     try {
       if (where) {
-        s.setAttribute("debug.location", where);
+        s.setAttribute("debug.file", where.fileName || "");
+        s.setAttribute("debug.line", where.lineNumber || 0);
+        s.setAttribute("debug.function", where.functionName || "");
       }
       return cb(s);
     } catch (e) {
